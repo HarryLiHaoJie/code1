7c8af2e22e3bdd6af873f715d57562eaa5608973
Liam <x.li@softwiseman.com>
2017-09-22 16:19:11 +0800

    修改邀请好友页面

--- src/components/invitation.vue
+++ src/components/invitation.vue
@@ -78,73 +78,79 @@
 
 		<!--/*********2017.8.4邀请好友*******/-->
 		<div class="cont1">
-			<img src="../assets/img/yaoqing.png"/>
+			<img src="../assets/img/1/1.png"/>
 			<div class="ewm">
 
 			</div>
 		</div>
-			<div class="cont2">
-			<img src="../assets/img/yaoqing2.png"/>
-			<!--<p class="inviteNum">好友扫描二维码即可分享邀请码: <span>{{ invit }}</span></p>-->
-			<p class="peoNum">{{ friend }}</p>
-			<p class="money">{{ money }}</p>
+			<div class="cont2" style="margin-top: -1px">
+			<!--<img src="../assets/img/yaoqing2.png"/>-->
+			<!--&lt;!&ndash;<p class="inviteNum">好友扫描二维码即可分享邀请码: <span>{{ invit }}</span></p>&ndash;&gt;-->
+			<div style="float: left;margin-left: 10px;margin-top: 60px;">
+				<img src="../assets/img/1/2.png" style="width: 170px;"/>
+			</div>
+			<div style="float: left;margin-left: 10px;margin-top: 60px;">
+				<img src="../assets/img/1/3.png" style="width: 170px;"/>
+			</div>
+			<p class="peoNum" style="left: 10%;top: 42%">{{ friend }}</p>
+			<p class="money" style="left: 58%;top: 42%">{{ money }}</p>
 
 			<router-link :to="{ path:'./reward_detail',query:{ source:this.$route.query.source,auth:this.$route.query.auth,username:this.$route.query.username }}">
-			    <span class="jilu">查看奖励明细>></span>
+			    <span class="jilu"><img src="../assets/img/1/0.png"/></span>
 			</router-link>
 		</div>
-		<div class="content3">
-			<img src="../assets/img/yaoqing3.png"/>
-			<div class="con-bottom">
-				<p>*邀请人奖励关系的有效期为：被邀请好友注册后30天，30天后奖励关系自动解除。</p>
-				<ul>
-					<li>您所赚奖金=好友投资金额x奖励系数</li>
-					<li class="clearfix">
-						<div class="li-left">
-							奖励条件
-						</div>
-						<div class="li-right">
-							奖励系数
-						</div>
-					</li>
-					<li class="clearfix fon">
-						<div class="li-left">投资新手专享</div>
-						<div class="li-right">0.03%</div>
-					</li>
-					<li class="clearfix fon">
-						<div class="li-left">投资月标</div>
-						<div class="li-right">0.1%</div>
-					</li>
-					<li class="clearfix fon">
-						<div class="li-left">投资半季标</div>
-						<div class="li-right">0.13%</div>
-					</li>
-					<li class="clearfix fon">
-						<div class="li-left">投资双月标</div>
-						<div class="li-right">0.15%</div>
-					</li>
-					<li class="clearfix fon">
-						<div class="li-left">投资季标</div>
-						<div class="li-right"> 0.2%</div>
-					</li>
-					<li class="clearfix fon" style="border-bottom: 0;">
-						<div class="li-left">投资双季标</div>
-						<div class="li-right"> 0.3%</div>
-					</li>
-				</ul>
-				<p style="margin-top: 10px;">例如:您邀请的好友在注册后30天内共投资货押宝双季标10万元，您所获得的奖励是300元  </p>
-			</div>
+		<div class="content3" style="margin-top: -1px">
+			<img src="../assets/img/1/4.png"/>
+			<!--<div class="con-bottom">-->
+				<!--<p>*邀请人奖励关系的有效期为：被邀请好友注册后30天，30天后奖励关系自动解除。</p>-->
+				<!--<ul>-->
+					<!--<li>您所赚奖金=好友投资金额x奖励系数</li>-->
+					<!--<li class="clearfix">-->
+						<!--<div class="li-left">-->
+							<!--奖励条件-->
+						<!--</div>-->
+						<!--<div class="li-right">-->
+							<!--奖励系数-->
+						<!--</div>-->
+					<!--</li>-->
+					<!--<li class="clearfix fon">-->
+						<!--<div class="li-left">投资新手专享</div>-->
+						<!--<div class="li-right">0.03%</div>-->
+					<!--</li>-->
+					<!--<li class="clearfix fon">-->
+						<!--<div class="li-left">投资月标</div>-->
+						<!--<div class="li-right">0.1%</div>-->
+					<!--</li>-->
+					<!--<li class="clearfix fon">-->
+						<!--<div class="li-left">投资半季标</div>-->
+						<!--<div class="li-right">0.13%</div>-->
+					<!--</li>-->
+					<!--<li class="clearfix fon">-->
+						<!--<div class="li-left">投资双月标</div>-->
+						<!--<div class="li-right">0.15%</div>-->
+					<!--</li>-->
+					<!--<li class="clearfix fon">-->
+						<!--<div class="li-left">投资季标</div>-->
+						<!--<div class="li-right"> 0.2%</div>-->
+					<!--</li>-->
+					<!--<li class="clearfix fon" style="border-bottom: 0;">-->
+						<!--<div class="li-left">投资双季标</div>-->
+						<!--<div class="li-right"> 0.3%</div>-->
+					<!--</li>-->
+				<!--</ul>-->
+				<!--<p style="margin-top: 10px;">例如:您邀请的好友在注册后30天内共投资货押宝双季标10万元，您所获得的奖励是300元  </p>-->
+			<!--</div>-->
 		</div>
-		<div class="content4">
-			<img src="../assets/img/yaoqing4.png"/>
-			<p>被邀请好友投资成功后，邀请人奖励会显示在【发现】>【邀请好友】>【查看奖励明细】中，当月奖励将于次月10号一次性发放至邀请人的新华金典账户，届时可直接用于投资或提现。</p>
+		<div class="content4" style="margin-top: -1px">
+			<img src="../assets/img/1/5.png"/>
+			<!--<p>被邀请好友投资成功后，邀请人奖励会显示在【发现】>【邀请好友】>【查看奖励明细】中，当月奖励将于次月10号一次性发放至邀请人的新华金典账户，届时可直接用于投资或提现。</p>-->
 		</div>
-		<div class="content5" style="height:390px;">
-			<img style="margin-bottom: 50px;" src="../assets/img/yaoqing5.png"/>
-			<p class="po-1">《中华人民共和国个人所得税法》第三条第四款规定“劳务报酬所得，适用比例税率，税率为百分之二十。对劳务报酬所得一次收入畸高的，可以实行加成征收，具体办法有国务院规定。”</p>
-			<p class="po-2">邀请人奖励将会根据平台的运营情况进行相应的调整，届时以公告通知为准。</p>
-			<p class="po-3">本活动最终解释权为新华金典所有。</p>
-		</div>
+		<!--<div class="content5" style="height:390px;">-->
+			<!--<img style="margin-bottom: 50px;" src="../assets/img/yaoqing5.png"/>-->
+			<!--<p class="po-1">《中华人民共和国个人所得税法》第三条第四款规定“劳务报酬所得，适用比例税率，税率为百分之二十。对劳务报酬所得一次收入畸高的，可以实行加成征收，具体办法有国务院规定。”</p>-->
+			<!--<p class="po-2">邀请人奖励将会根据平台的运营情况进行相应的调整，届时以公告通知为准。</p>-->
+			<!--<p class="po-3">本活动最终解释权为新华金典所有。</p>-->
+		<!--</div>-->
 
 		</div>
 </template>
